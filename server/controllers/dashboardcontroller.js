462d5a4f49b4c0482467b3bbdb217f582f291e10
sudhanshu808-in <shudhanshukaprowan@gmail.com>
2024-09-27 01:35:24 +0530

    dashboard

--- server/controllers/dashboardcontroller.js
+++ server/controllers/dashboardcontroller.js
@@ -7,28 +7,27 @@ const mongoose = require('mongoose');
 
 exports.dashboard = async (req, res) => {
 
-    try {
-      await Note.insertMany([
-        {
-          user : '66f5924e0022faa209fd9791'
-        }
-      ])
-    } catch (error) {
-      
-    }
-
-
-
 
 
     const locals = {
       title: "Dashboard",
       description: "dashboard....",
     }
-    res.render('dashboard/index', {
-      userName : req.user.firstName,
-      locals,
-      layout: '../views/layouts/dashboard'
-    }); 
+
+
+try {
+  const notes = await Note.find({})
+  
+      res.render('dashboard/index', {
+        userName : req.user.firstName,
+        locals,
+        notes,
+        layout: '../views/layouts/dashboard'
+      }); 
+  
+} catch (error) {
+  
+}
+
   }
   
