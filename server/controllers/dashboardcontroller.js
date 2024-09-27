2b8396cdbe9c0ce4e88630cb93768dcbb8632e0e
sudhanshu808-in <shudhanshukaprowan@gmail.com>
2024-09-27 13:11:32 +0530

    dashboard_updates

--- server/controllers/dashboardcontroller.js
+++ server/controllers/dashboardcontroller.js
@@ -7,27 +7,55 @@ const mongoose = require('mongoose');
 
 exports.dashboard = async (req, res) => {
 
+  let perPage = 12;
+  let page = req.query.page || 1
+
+  const locals = {
+    title: "Dashboard",
+    description: "dashboard....",
+  }
 
 
-    const locals = {
-      title: "Dashboard",
-      description: "dashboard....",
-    }
+  try {
 
 
-try {
-  const notes = await Note.find({})
-  
-      res.render('dashboard/index', {
-        userName : req.user.firstName,
-        locals,
-        notes,
-        layout: '../views/layouts/dashboard'
-      }); 
-  
-} catch (error) {
-  
-}
+    Note.aggregate([
+      {
+        $sort: {
+          createdAt: 1,
+        }
+      },
+      { $match: { user: mongoose.Types.ObjectId(req.user.id) } },
+      {
+        $project: {
+          title: { $substr: ['$title', 0, 30] },
+          body: { $substr: ['$body', 0, 100] },
+
+
+        }
+      }
+
+    ]).skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec((err, notes) => {
+        Note.count().exec((err, count) => {
+          if (err) return next(err);
+
+
+
+          res.render('dashboard/index', {
+            userName: req.user.firstName,
+            locals,
+            notes,
+            layout: '../views/layouts/dashboard',
+            current: page,
+            pages: Math.ceil(count / perPage)
+          });
+        })
+      })
+  } catch (error) {
+    console.log(error);
 
   }
-  
+
+}
