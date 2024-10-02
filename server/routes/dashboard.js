6cd97810b4f0036e11dc7cdc0bdfa96df00ae764
sudhanshu808-in <shudhanshukaprowan@gmail.com>
2024-10-03 04:40:13 +0530

    updates

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -7,6 +7,11 @@ const { isLoggedIn }=require('../middleware/chechAuth');
 router.get('/dashboard',isLoggedIn,dashboardcontroller.dashboard);
 router.get('/dashboard/item/:id',isLoggedIn,dashboardcontroller.dashboardViewNote);
 router.post('/dashboard/item/:id',isLoggedIn,dashboardcontroller.dashboardUpdateNote);
+router.post('/dashboard/item-delete/:id',isLoggedIn,dashboardcontroller.dashboardDeleteNote);
+router.get('/dashboard/add',isLoggedIn,dashboardcontroller.dashboardAddNote);
+router.post('/dashboard/add',isLoggedIn,dashboardcontroller.dashboardAddNoteSubmit);
+router.get('/dashboard/search',isLoggedIn,dashboardcontroller.dashboardSearch);
+router.post('/dashboard/search',isLoggedIn,dashboardcontroller.dashboardSearchSubmit);
 
 module.exports = router;
 
